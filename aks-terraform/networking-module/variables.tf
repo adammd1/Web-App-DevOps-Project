ecd91641e3dcbf60c9525b5eccfb5b07a162bbbb
adammd1 <adam.mahiddine@gmail.com>
2024-01-04 19:21:43 +0000

    adding all my files including everything inside the aks-terraform directory and my application-manifest.yaml

---
+++ aks-terraform/networking-module/variables.tf
@@ -0,0 +1,19 @@
+# networking-module/variables.tf
+
+variable "resource_group_name" {
+    description = "Name of Azure Resource Group"
+    type        = string
+    default     = "aks-rg"
+}
+
+variable "location" {
+    description = "Name of Azure Region"
+    type        = string
+    default     = "UK South"
+}
+
+variable "vnet_address_space" {
+  description = "Address space for Virtual Network."
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
